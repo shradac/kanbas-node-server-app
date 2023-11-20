4bb148a6485d819b258993fc9043d5a33451868d
Shrada Chellasami <shradac@gmail.com>
2023-11-20 16:40:40 -0500

    node server lab

---
+++ app.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import Hello from "./hello.js";
+import Lab5 from "./lab5.js";
+import cors from "cors";
+
+
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+Lab5(app);
+Hello(app);
+
+
+app.listen(4000);
---
+++ hello.js
@@ -0,0 +1,19 @@
+// console.log("Hello World!");
+
+// app.get('/hello', (req, res) => {
+//     res.send('Life is good!')
+//  });
+//  app.get('/', (req, res) => {
+//     res.send('Welcome to Full Stack Development!')
+//  });
+ 
+
+const Hello = (app) => {
+    app.get('/hello', (req, res) => {
+      res.send('Life is good!')
+    })
+    app.get('/', (req, res) => {
+      res.send('Welcome to Full Stack Development!')
+    })
+  }
+  export default Hello;
