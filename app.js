1c23e5faf46f5d25a2000055c54126bec7e9139a
Shrada Chellasami <shradac@gmail.com>
2023-11-20 17:18:37 -0500

    lab

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-import express from 'express';
+import "dotenv/config";
+import express from "express";
 import Hello from "./hello.js";
-import Lab5 from "./lab5.js";
+import CourseRoutes from "./courses/routes.js";
+import ModuleRoutes from "./modules/routes.js";
 import cors from "cors";
-
-
-
+import Lab5 from "./lab5.js";
 const app = express();
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
+CourseRoutes(app);
+ModuleRoutes(app);
 Lab5(app);
 Hello(app);
-
-
-app.listen(4000);
+app.listen(process.env.PORT || 4000);
