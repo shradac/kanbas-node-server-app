4a7fdea38e25c0d39ad05bec1d871b252b17c6d6
Shrada Chellasami <shradac@gmail.com>
2023-12-04 16:21:19 -0500

    assignment 6

--- app.js
+++ app.js
@@ -5,8 +5,31 @@ import CourseRoutes from "./courses/routes.js";
 import ModuleRoutes from "./modules/routes.js";
 import cors from "cors";
 import Lab5 from "./lab5.js";
+import UserRoutes from "./users/routes.js";
+import session from "express-session";
+import mongoose from "mongoose";
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas"
+mongoose.connect(CONNECTION_STRING);
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.FRONTEND_URL,
+  })
+);
+const sessionOptions = {
+  secret: "any string",
+  resave: false,
+  saveUninitialized: false,
+};
+if (process.env.NODE_ENV !== "development") {
+  sessionOptions.proxy = true;
+  sessionOptions.cookie = {
+    sameSite: "none",
+    secure: true,
+  };
+}
+app.use(session(sessionOptions));
 app.use(express.json());
 CourseRoutes(app);
 ModuleRoutes(app);
